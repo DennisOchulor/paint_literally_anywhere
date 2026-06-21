fe69e068cc902278d4d93eb8d6357fdee7d91c2c
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-06-21 16:22:04 +0800

    add mirror block

--- src/main/java/io/github/dennisochulor/playground/Playground.java
+++ src/main/java/io/github/dennisochulor/playground/Playground.java
@@ -14,6 +14,8 @@ public class Playground implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initialising Playground!");
+
+		PlaygroundBlocks.init();
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/io/github/dennisochulor/playground/PlaygroundBlocks.java
@@ -0,0 +1,64 @@
+package io.github.dennisochulor.playground;
+
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+import java.util.function.Function;
+
+public class PlaygroundBlocks {
+    public static final Block MIRROR = register(
+            "mirror",
+            Block::new,
+            BlockBehaviour.Properties.of()
+                    .sound(SoundType.GLASS)
+                    .strength(0.3F)
+                    .isValidSpawn(Blocks::never)
+                    .isRedstoneConductor(Blocks::never)
+                    .isSuffocating(Blocks::never),
+            true
+    );
+
+    public static void init() {
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(tab -> {
+            tab.accept(MIRROR.asItem());
+        });
+    }
+
+    private static Block register(String name, Function<BlockBehaviour.Properties, Block> blockFactory, BlockBehaviour.Properties properties, boolean shouldRegisterItem) {
+        // Create a registry key for the block
+        ResourceKey<Block> blockKey = keyOfBlock(name);
+        // Create the block instance
+        Block block = blockFactory.apply(properties.setId(blockKey));
+
+        // Sometimes, you may not want to register an item for the block.
+        // Eg: if it's a technical block like `minecraft:moving_piston` or `minecraft:end_gateway`
+        if (shouldRegisterItem) {
+            // Items need to be registered with a different type of registry key, but the ID
+            // can be the same.
+            ResourceKey<Item> itemKey = keyOfItem(name);
+
+            BlockItem blockItem = new BlockItem(block, new Item.Properties().setId(itemKey).useBlockDescriptionPrefix());
+            Registry.register(BuiltInRegistries.ITEM, itemKey, blockItem);
+        }
+
+        return Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
+    }
+
+    private static ResourceKey<Block> keyOfBlock(String name) {
+        return ResourceKey.create(Registries.BLOCK, Playground.id(name));
+    }
+
+    private static ResourceKey<Item> keyOfItem(String name) {
+        return ResourceKey.create(Registries.ITEM, Playground.id(name));
+    }
+}
