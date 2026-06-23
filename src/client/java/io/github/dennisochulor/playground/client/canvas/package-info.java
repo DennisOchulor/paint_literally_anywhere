34e06d6986e5cd15682d63155770a11b01a0095c
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-06-23 10:51:28 +0800

    package-infos

---
+++ src/client/java/io/github/dennisochulor/playground/client/canvas/package-info.java
@@ -0,0 +1,5 @@
+
+@NullMarked
+package io.github.dennisochulor.playground.client.canvas;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/io/github/dennisochulor/playground/canvas/package-info.java
@@ -0,0 +1,5 @@
+
+@NullMarked
+package io.github.dennisochulor.playground.canvas;
+
+import org.jspecify.annotations.NullMarked;
