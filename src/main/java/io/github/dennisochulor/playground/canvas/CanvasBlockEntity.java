01ae99932209167d7bf39c2667e731788f26811f
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-06-24 19:28:28 +0800

    remove import

--- src/main/java/io/github/dennisochulor/playground/canvas/CanvasBlockEntity.java
+++ src/main/java/io/github/dennisochulor/playground/canvas/CanvasBlockEntity.java
@@ -11,7 +11,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.Objects;
 
