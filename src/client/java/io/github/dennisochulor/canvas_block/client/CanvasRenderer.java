c80f2c9891e0117426ce91b716bafa044a63bec4
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-07-02 15:21:21 +0800

    enable culling and fix UP face culling issue

--- src/client/java/io/github/dennisochulor/canvas_block/client/CanvasRenderer.java
+++ src/client/java/io/github/dennisochulor/canvas_block/client/CanvasRenderer.java
@@ -15,7 +15,6 @@ import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.rendertype.LayeringTransform;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
 import net.minecraft.client.renderer.rendertype.RenderType;
-import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.core.Direction;
 import net.minecraft.util.ARGB;
@@ -31,17 +30,29 @@ public class CanvasRenderer implements BlockEntityRenderer<CanvasBlockEntity, Ca
     private static final float STEP = 1.0F / CanvasBlock.SIZE;
 
     // vertex shader copied from position_color.vsh and text_background.vsh
-    private static final RenderPipeline FILLED_BOX_PIPELINE = RenderPipelines.register(
-            RenderPipeline.builder(RenderPipelines.DEBUG_FILLED_SNIPPET).withLocation(CanvasMod.id("pipeline/filled_box"))
+    private static final RenderPipeline NON_EMISSIVE_FILLED_BOX_PIPELINE = RenderPipelines.register(
+            RenderPipeline.builder(RenderPipelines.DEBUG_FILLED_SNIPPET).withLocation(CanvasMod.id("pipeline/non_emissive_filled_box"))
                     .withVertexShader(CanvasMod.id("core/position_color_lightmap"))
                     .withBindGroupLayout(BindGroupLayouts.SAMPLER2)
-                    //.withCull(true) // causes top/bottom to not render for some reason...
+                    .withCull(true)
                     .withVertexBinding(0, DefaultVertexFormat.POSITION_COLOR_LIGHTMAP)
                     .build()
     );
 
-    private static final RenderType FILLED_BOX_TYPE = RenderType.create("canvas_filled_box",
-            RenderSetup.builder(FILLED_BOX_PIPELINE).sortOnUpload().useLightmap().setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING).createRenderSetup());
+    private static final RenderType NON_EMISSIVE_FILLED_BOX_TYPE = RenderType.create("canvas_non_emissive_filled_box",
+            RenderSetup.builder(NON_EMISSIVE_FILLED_BOX_PIPELINE).sortOnUpload().useLightmap().setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING).createRenderSetup());
+
+
+    private static final RenderPipeline EMISSIVE_FILLED_BOX_PIPELINE = RenderPipelines.register(
+            RenderPipeline.builder(RenderPipelines.DEBUG_FILLED_SNIPPET).withLocation(CanvasMod.id("pipeline/emissive_filled_box"))
+                    .withCull(true)
+                    .build()
+    );
+
+    private static final RenderType EMISSIVE_FILLED_BOX_TYPE = RenderType.create("canvas_emissive_filled_box",
+            RenderSetup.builder(EMISSIVE_FILLED_BOX_PIPELINE).sortOnUpload().setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING).createRenderSetup());
+
+
 
     @SuppressWarnings("unused")
     public CanvasRenderer(BlockEntityRendererProvider.Context context) {}
@@ -114,22 +125,41 @@ public class CanvasRenderer implements BlockEntityRenderer<CanvasBlockEntity, Ca
                 float xBase = xRowStep != 0 ? row * xRowStep : col * xColStep;
                 float yBase = yRowStep != 0 ? row * yRowStep : col * yColStep;
                 float zBase = zRowStep != 0 ? row * zRowStep : col * zColStep;
-                submitNodeCollector.submitCustomGeometry(poseStack, emissive ? RenderTypes.debugFilledBox() : FILLED_BOX_TYPE, (pose, buffer) -> {
-                    // base
-                    buffer.addVertex(pose, xBase, yBase, zBase)
-                            .setColor(color).setLight(lightCoords).setUv(0, 1).setLineWidth(1);
+                submitNodeCollector.submitCustomGeometry(poseStack, emissive ? EMISSIVE_FILLED_BOX_TYPE : NON_EMISSIVE_FILLED_BOX_TYPE, (pose, buffer) -> {
+                    if (side == UP) { // UP needs a different vertex winding order to not be culled for some reason idk man...
+                        // base
+                        buffer.addVertex(pose, xBase, yBase, zBase)
+                                .setColor(color).setLight(lightCoords).setUv(0, 1).setLineWidth(1);
 
-                    // step row
-                    buffer.addVertex(pose, xBase + xRowStep, yBase + yRowStep, zBase + zRowStep)
-                            .setColor(color).setLight(lightCoords).setUv(1, 1).setLineWidth(1);
+                        // step col
+                        buffer.addVertex(pose, xBase + xColStep, yBase + yColStep, zBase + zColStep)
+                                .setColor(color).setLight(lightCoords).setUv(0, 0).setLineWidth(1);
 
-                    // step row, step col
-                    buffer.addVertex(pose, xBase + xRowStep + xColStep, yBase + yRowStep + yColStep, zBase + zRowStep + zColStep)
-                            .setColor(color).setLight(lightCoords).setUv(1, 0).setLineWidth(1);
+                        // step row, step col
+                        buffer.addVertex(pose, xBase + xRowStep + xColStep, yBase + yRowStep + yColStep, zBase + zRowStep + zColStep)
+                                .setColor(color).setLight(lightCoords).setUv(1, 0).setLineWidth(1);
 
-                    // step col
-                    buffer.addVertex(pose, xBase + xColStep, yBase + yColStep, zBase + zColStep)
-                            .setColor(color).setLight(lightCoords).setUv(0, 0).setLineWidth(1);
+                        // step row
+                        buffer.addVertex(pose, xBase + xRowStep, yBase + yRowStep, zBase + zRowStep)
+                                .setColor(color).setLight(lightCoords).setUv(1, 1).setLineWidth(1);
+                    }
+                    else {
+                        // base
+                        buffer.addVertex(pose, xBase, yBase, zBase)
+                                .setColor(color).setLight(lightCoords).setUv(0, 1).setLineWidth(1);
+
+                        // step row
+                        buffer.addVertex(pose, xBase + xRowStep, yBase + yRowStep, zBase + zRowStep)
+                                .setColor(color).setLight(lightCoords).setUv(1, 1).setLineWidth(1);
+
+                        // step row, step col
+                        buffer.addVertex(pose, xBase + xRowStep + xColStep, yBase + yRowStep + yColStep, zBase + zRowStep + zColStep)
+                                .setColor(color).setLight(lightCoords).setUv(1, 0).setLineWidth(1);
+
+                        // step col
+                        buffer.addVertex(pose, xBase + xColStep, yBase + yColStep, zBase + zColStep)
+                                .setColor(color).setLight(lightCoords).setUv(0, 0).setLineWidth(1);
+                    }
                 });
             }
         }
