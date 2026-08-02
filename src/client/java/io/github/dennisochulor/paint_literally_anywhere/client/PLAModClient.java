f968cae259ff12a33f60011a40e6aba94894bd40
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-08-02 15:22:29 +0800

    paintbrush tooltip

--- src/client/java/io/github/dennisochulor/paint_literally_anywhere/client/PLAModClient.java
+++ src/client/java/io/github/dennisochulor/paint_literally_anywhere/client/PLAModClient.java
@@ -5,15 +5,19 @@ import io.github.dennisochulor.paint_literally_anywhere.client.model.ModModelLoa
 import io.github.dennisochulor.paint_literally_anywhere.item.ModComponents;
 import io.github.dennisochulor.paint_literally_anywhere.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.event.player.ItemEvents;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.item.ItemTintSources;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.ARGB;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 
 import java.awt.Color;
+import java.util.Locale;
 
 public class PLAModClient implements ClientModInitializer {
 
@@ -36,6 +40,16 @@ public class PLAModClient implements ClientModInitializer {
 
             Minecraft.getInstance().gui.setScreen(new CreativeColorPickerScreen(new Color(argb, true), emissive));
             return InteractionResult.CONSUME;
+        });
+
+        ItemTooltipCallback.EVENT.register((stack, _, _, lines) -> {
+            if (stack.get(ModComponents.ARGB_COLOR) instanceof Integer argb) {
+                int opacity = (int) Math.round(ARGB.alpha(argb) / 255.0 * 100);
+                String hex = "#" + Integer.toHexString(argb).substring(2).toUpperCase(Locale.ROOT);
+
+                lines.add(Component.literal("Color: ").append(Component.literal(hex).withColor(argb)));
+                lines.add(Component.literal("Opacity: " + opacity + "%"));
+            }
         });
     }
 }
