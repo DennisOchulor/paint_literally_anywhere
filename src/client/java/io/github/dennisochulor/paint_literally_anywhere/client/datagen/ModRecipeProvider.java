b1a2516b6c9eefed1a72e3b776fcdb95adbcf606
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-08-31 20:29:24 +0800

    remove emissive recipe and tweak normal recipe

--- src/client/java/io/github/dennisochulor/paint_literally_anywhere/client/datagen/ModRecipeProvider.java
+++ src/client/java/io/github/dennisochulor/paint_literally_anywhere/client/datagen/ModRecipeProvider.java
@@ -1,22 +1,15 @@
 package io.github.dennisochulor.paint_literally_anywhere.client.datagen;
 
-import io.github.dennisochulor.paint_literally_anywhere.PLAMod;
-import io.github.dennisochulor.paint_literally_anywhere.item.ModComponents;
 import io.github.dennisochulor.paint_literally_anywhere.item.ModItems;
-import io.github.dennisochulor.paint_literally_anywhere.item.PaintBrushProperties;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.TransmuteRecipeBuilder;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStackTemplate;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -33,27 +26,15 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 //noinspection unused
                 HolderLookup.RegistryLookup<Item> itemLookup = registries.lookupOrThrow(Registries.ITEM);
 
-                shaped(RecipeCategory.MISC, ModItems.PAINT_BRUSH)
+                shaped(RecipeCategory.TOOLS, ModItems.PAINT_BRUSH)
                         .define('F', Items.FEATHER)
                         .define('S', Items.STICK)
-                        .pattern(" F ")
+                        .pattern("FFF")
                         .pattern(" S ")
                         .pattern(" S ")
                         .unlockedBy(getHasName(Items.FEATHER), has(Items.FEATHER))
                         .group("paint_brush")
                         .save(output);
-
-                // todo figure out how to not override all PaintBrushProperties fields
-                TransmuteRecipeBuilder.transmute(
-                        RecipeCategory.MISC,
-                        Ingredient.of(ModItems.PAINT_BRUSH),
-                        Ingredient.of(Items.GLOW_INK_SAC),
-                        new ItemStackTemplate(ModItems.PAINT_BRUSH, DataComponentPatch.builder().set(ModComponents.PAINT_BRUSH, PaintBrushProperties.DEFAULT.withEmissive(true)).build())
-                )
-                        .unlockedBy(getHasName(Items.GLOW_INK_SAC), has(Items.GLOW_INK_SAC))
-                        .unlockedBy(getHasName(ModItems.PAINT_BRUSH), has(ModItems.PAINT_BRUSH))
-                        .group("paint_brush")
-                        .save(output, PLAMod.MOD_ID + ":paint_brush_emissive");
             }
         };
     }
