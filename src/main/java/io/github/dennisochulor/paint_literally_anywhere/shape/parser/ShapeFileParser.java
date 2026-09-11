0617b2b915504ffe714778fc23b28745c9fd1e10
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-09-11 19:46:17 +0800

    include namespace version in shape filename

--- src/main/java/io/github/dennisochulor/paint_literally_anywhere/shape/parser/ShapeFileParser.java
+++ src/main/java/io/github/dennisochulor/paint_literally_anywhere/shape/parser/ShapeFileParser.java
@@ -1,6 +1,7 @@
 package io.github.dennisochulor.paint_literally_anywhere.shape.parser;
 
 import io.github.dennisochulor.paint_literally_anywhere.PLAMod;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.nbt.NbtAccounter;
 import net.minecraft.nbt.NbtIo;
 import net.minecraft.nbt.NbtOps;
@@ -10,15 +11,17 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public final class ShapeFileParser {
     private ShapeFileParser() {}
 
     public static final String RELATIVE_PATH_TO_SHAPES_DIR = "config/paint_literally_anywhere/shapes/";
+    public static final char FILENAME_SEPARATOR = '@';
+    public static final Pattern FILENAME_PATTERN = Pattern.compile("(.+?)" + FILENAME_SEPARATOR + "(.+)\\.dat");
 
     public static ShapeFileParseResult parse(Path rootDirectory) {
         Path shapesFolder = rootDirectory.resolve(RELATIVE_PATH_TO_SHAPES_DIR);
@@ -28,7 +31,35 @@ public final class ShapeFileParser {
 
         List<Path> shapeFiles;
         try (Stream<Path> stream = Files.list(shapesFolder)) {
-            shapeFiles = stream.toList();
+            Map<String, Path> namespacesSeen = new HashMap<>();
+
+            stream.forEach(path -> {
+                String filename = path.getFileName().toString();
+                Matcher matcher = FILENAME_PATTERN.matcher(filename);
+
+                if (matcher.matches()) {
+                    String namespace = matcher.group(1);
+                    String version = matcher.group(2);
+
+                    FabricLoader.getInstance().getModContainer(namespace).ifPresentOrElse(modContainer -> {
+                        // If there is never any exact match for a namespace, then it will just use the first one.
+                        boolean isExactMatch = version.equals(modContainer.getMetadata().getVersion().getFriendlyString());
+
+                        if (!namespacesSeen.containsKey(namespace) || isExactMatch) {
+                            namespacesSeen.put(namespace, path);
+                        }
+                    },
+
+                    () -> {
+                        PLAMod.LOGGER.warn("Cannot find matching ModContainer for namespace {}", namespace);
+                    });
+                }
+                else {
+                    PLAMod.LOGGER.warn("Ignoring unknown shape file: {}", filename);
+                }
+            });
+
+            shapeFiles = namespacesSeen.values().stream().toList();
         }
         catch (IOException e) {
             throw new UncheckedIOException(e);
@@ -37,6 +68,10 @@ public final class ShapeFileParser {
         if (shapeFiles.isEmpty()) {
             return ShapeFileParseResult.EMPTY;
         }
+
+        StringBuilder sb = new StringBuilder();
+        shapeFiles.forEach(path -> sb.append(path.getFileName().toString()).append(", "));
+        PLAMod.LOGGER.debug("About to parse {}", sb);
 
         long startTime = Util.getMillis();
         Map<String, ShapeFile> map = new HashMap<>();
@@ -47,7 +82,13 @@ public final class ShapeFileParser {
                             ShapeFile record = pair.getFirst();
                             map.put(record.namespace(), record);
                         })
-                        .ifError(err -> PLAMod.LOGGER.warn("Failed to parse shape file {}\n{}", path.getFileName(), err.message()));
+                        .ifError(err -> {
+                            PLAMod.LOGGER.warn("Encountered errors while parsing shape file {}\n{}", path.getFileName(), err.message());
+                            err.resultOrPartial().ifPresent(pair -> {
+                                ShapeFile record = pair.getFirst();
+                                map.put(record.namespace(), record);
+                            });
+                        });
             }
             catch (IOException e) {
                 PLAMod.LOGGER.warn("Failed to read shape file {}", path.getFileName(), e);
